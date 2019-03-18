394c8097941bd2fd7e77ae96dc8c6fd4295a74a0
Christian Estepa Garcia <christian.estepa@MasMovil.com>
2019-03-18 10:51:23 +0100

    fetch products

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -7,45 +7,73 @@ import {
     SAVE_PRODUCT_FAILURE
 } from './types';
 
+import API from '../api';
+
 // ACTION CREATORS
-export function fetchProductsSuccess (products) {
+export function fetchProductsSuccess(products) {
     return {
         type: FETCH_PRODUCTS_SUCCESS,
         payload: products
     };
 }
 
-export function fetchProductsFailure (error) {
+export function fetchProductsFailure(error) {
     return {
         type: FETCH_PRODUCTS_FAILURE,
         payload: error
     };
 }
 
-export function fetchProductSuccess (product) {
+export function fetchProductSuccess(product) {
     return {
         type: FETCH_PRODUCT_SUCCESS,
         payload: product
     };
 }
 
-export function fetchProductFailure (error) {
+export function fetchProductFailure(error) {
     return {
         type: FETCH_PRODUCT_FAILURE,
         payload: error
     };
 }
 
-export function saveProductSuccess () {
+export function saveProductSuccess() {
     return {
         type: SAVE_PRODUCT_SUCCESS
     };
 }
-E
-export function saveProductFailure (error) {
+
+export function saveProductFailure(error) {
     return {
         type: SAVE_PRODUCT_FAILURE,
         payload: error
     };
 }
 
+// ACTION CREATORS (ASYNC)
+export function fetchProducts() {
+    return async(dispatch => {
+        dispatch(() => {
+            return {
+                type: 'FETCH_PRODUCT_INIT'
+            }
+        })
+
+        try {
+            const data = await API.products.getAll();
+            return dispatch(fetchProductsSuccess(data.products))
+        }
+        catch (error) {
+            return dispatch(fetchProductsFailure(error))
+        }
+    })
+}
+
+export function fetchProduct() {
+
+}
+
+export function saveProduct() {
+
+}
