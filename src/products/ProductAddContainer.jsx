e8433efd5a9dc8982a3da96206fcc4fd09de60c0
Christian <christian@MacBook-Pro-de-admindesk.local>
2019-03-27 16:28:31 +0100

    productaddcontainer init

---
+++ src/products/ProductAddContainer.jsx
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { browserHistory } from 'react-router';
+
+import * as productActions from '../actions/productActions';
+
+class ProductAddContainer extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+
+    }
+}
+
+export default ProductAddContainer;
