62b8dae2140a08c6acbb982a8853ff8fed328e1c
Christian <christian@MacBook-Pro-de-admindesk.local>
2019-03-27 16:13:07 +0100

    product detail container not working

--- src/products/ProductDetail.jsx
+++ src/products/ProductDetail.jsx
@@ -42,7 +42,8 @@ const ProductDetail = ({ loading, product, onAddItem }) => {
 
 ProductDetail.propTypes = {
     product: PropTypes.object,
-    loading: PropTypes.boolean.isRequired
+    loading: PropTypes.bool.isRequired,
+    onAddItem: PropTypes.func.isRequired
 };
 
 export default ProductDetail;
